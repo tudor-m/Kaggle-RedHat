562fdb7f6ac0d60b38995a055c6e283d5e007f91
tudor-m <tudor.morosan@yahoo.com>
2016-09-05 20:18:52 -0400

    wip

--- R/LoadAndCombine.R
+++ R/LoadAndCombine.R
@@ -4,13 +4,22 @@ inDataFiles = list()
 
 inDataFiles[[1]] = "../public/0.990953/mod3108Kaggle_01-2.csv"
 inDataFiles[[2]] = "../public/0.990949/mod3108Kaggle_01.csv"
+inDataFiles[[3]] = "../public/0.990970/mod3108Kaggle_01.csv"
+inDataFiles[[4]] = "../public/0.990971/mod3108Kaggle_01.csv"
+
 kod = 1 #0.990953
 kod = 2 #0.990953
+kod = 3 #0.990970
+kod = 4
 
 if (kod == 1)
   inCoef = c(0.8,0.2)
 if (kod == 2)
   inCoef = c(0.5,0.5)
+if (kod == 3)
+  inCoef = c(0.3,0.2,0.5)
+if (kod == 4)
+  inCoef = c(0.05,0.05,0.4,0.5)
 
 for (i in 1:length(inDataFiles))
 {
@@ -20,14 +29,16 @@ for (i in 1:length(inDataFiles))
 allData = inDataTables[[1]]
 for (i in 2:length(inDataTables))
 {
-  allData = merge(allData,inDataTables[[2]],by="activity_id")
+  allData = merge(allData,inDataTables[[i]],by="activity_id")
 }
 
 # add white noise and combine data
 set.seed(100)
 #noise = rnorm(nrow(allData),mean=1,sd=0.0001) # noise 0.990954
 #noise = rnorm(nrow(allData),mean=1,sd=0.001)# noise2 0.990954 best so far
-noise = rnorm(nrow(allData),mean=1,sd=0.01)# noise3 0.990952
+#noise = rnorm(nrow(allData),mean=1,sd=0.01)# noise3 0.990952
+noise = 1+0*rnorm(nrow(allData),mean=1,sd=0.001)# noise4
+
 combinedData = 0
 for (i in 1:length(inCoef))
 {
@@ -38,7 +49,7 @@ submitData = as.data.table(cbind(allData$activity_id,combinedData))
 setnames(submitData,c("activity_id","outcome"))
 options(scipen = 999)
 
-subFileName = paste(c("submit.LoadAndCombine.kod.",kod,".noise3.csv"),sep = "",collapse = "")
+subFileName = paste(c("submit.LoadAndCombine.kod.",kod,".noise0.2.csv"),sep = "",collapse = "")
 write.csv(submitData[,.(activity_id,outcome)],subFileName, row.names = FALSE)
 
 options(scipen = 0)
