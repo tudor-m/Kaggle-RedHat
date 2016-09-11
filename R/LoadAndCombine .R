83600e4b69c229ff27e4eb0519060827eac7bf89
tudor-m <tudor.morosan@yahoo.com>
2016-09-10 20:31:53 -0400

    LB: 0.991687 kod5.noise4.1.csv

--- R/LoadAndCombine .R
+++ R/LoadAndCombine .R
@@ -6,11 +6,13 @@ inDataFiles[[1]] = "../public/0.990953/mod3108Kaggle_01-2.csv"
 inDataFiles[[2]] = "../public/0.990949/mod3108Kaggle_01.csv"
 inDataFiles[[3]] = "../public/0.990970/mod3108Kaggle_01.csv"
 inDataFiles[[4]] = "../public/0.990971/mod3108Kaggle_01.csv"
+inDataFiles[[5]] = "../public/0.991647/mod3108Kaggle_01.csv"
 
 kod = 1 #0.990953
 kod = 2 #0.990953
 kod = 3 #0.990970
 kod = 4
+kod = 5
 
 if (kod == 1)
   inCoef = c(0.8,0.2)
@@ -20,10 +22,13 @@ if (kod == 3)
   inCoef = c(0.3,0.2,0.5)
 if (kod == 4)
   inCoef = c(0.0,0.0,0.4,.6)
+if (kod == 5)
+  inCoef = c(0.0,0.0,0.0,.2,0.8)
 
 for (i in 1:length(inDataFiles))
 {
   inDataTables[[i]] <- fread(inDataFiles[[i]], header=TRUE)
+  setnames((inDataTables[[i]]),c("activity_id",i))
 }
 
 
@@ -50,7 +55,7 @@ submitData = as.data.table(cbind(allData$activity_id,combinedData))
 setnames(submitData,c("activity_id","outcome"))
 options(scipen = 999)
 
-subFileName = paste(c("submit.LoadAndCombine.kod.",kod,".noise4.7.csv"),sep = "",collapse = "")
+subFileName = paste(c("submit.LoadAndCombine.kod.",kod,".noise4.1.csv"),sep = "",collapse = "")
 write.csv(submitData[,.(activity_id,outcome)],subFileName, row.names = FALSE)
 
 options(scipen = 0)
