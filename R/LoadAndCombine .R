637d112c3798f59defb09b083805bc0be07f0f22
tudor-m <tudor.morosan@yahoo.com>
2016-09-18 09:48:04 -0400

    PB: 0.991743 kod 12 noise 0.2

--- R/LoadAndCombine .R
+++ R/LoadAndCombine .R
@@ -53,7 +53,7 @@ if (kod == 10)
 if (kod == 11)
   inCoef = c(0.0,0.0,0.0,.2,0.0,0.2,0.0,0.0,0.2,0.2,0.2)
 if (kod == 12)
-  inCoef = c(0.0,0.0,0.0,.17,0.0,0.16,0.0,0.0,0.16,0.17,0.17,0.17)
+  inCoef = c(0.0,0.0,0.0,.2,0.0,0.2,0.0,0.0,0.0,0.2,0.2,0.2)
 
 for (i in 1:length(inDataFiles))
 {
@@ -87,7 +87,7 @@ submitData = as.data.table(cbind(allData$activity_id,combinedData))
 setnames(submitData,c("activity_id","outcome"))
 options(scipen = 999)
 
-subFileName = paste(c("submit.LoadAndCombine.kod.",kod,".noise0.1.csv"),sep = "",collapse = "")
+subFileName = paste(c("submit.LoadAndCombine.kod.",kod,".noise0.2.csv"),sep = "",collapse = "")
 write.csv(submitData[,.(activity_id,outcome)],subFileName, row.names = FALSE)
 
 options(scipen = 0)
